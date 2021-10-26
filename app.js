3ed3a8f986a1d68b825598ca9ea6fc336804c2d3
Yukiko <yukikogimi@gmail.com>
2021-10-26 22:43:23 +1100

    css added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
   host: "localhost",
